bfc8ef07d5bbe4302682ed2221cc21c28369a52e
Kaue Batista <83407339+KaueB@users.noreply.github.com>
2021-12-19 22:55:21 -0300

    domingo de noite

--- desafio/desafio01react/meu-app-react/src/main.jsx
+++ desafio/desafio01react/meu-app-react/src/main.jsx
@@ -1,10 +1,12 @@
 import './style/main.css';
+import bugdroid from './img/bugdroid.png'
 
 function Main() {
     return (
         <div id='conteudo'>
             <aside>
                 <h1>História do Mascote do Android</h1>
+                <img src={bugdroid}/>
                 <p>Provavelmente você sabe que o sistema operacional Android, mantido pelo Google é um dos mais utilizados para dispositivos móveis em todo o mundo. Mas tavez você não saiba que o seu simpático mascote tem um nome e uma história muito curiosa? Pois acompanhe esse artigo para aprender muita coisa sobre esse robozinho.</p>
                 <p>Provavelmente você sabe que o sistema operacional Android, mantido pelo Google é um dos mais utilizados para dispositivos móveis em todo o mundo. Mas tavez você não saiba que o seu simpático mascote tem um nome e uma história muito curiosa? Pois acompanhe esse artigo para aprender muita coisa sobre esse robozinho.</p>
                 <p>Provavelmente você sabe que o sistema operacional Android, mantido pelo Google é um dos mais utilizados para dispositivos móveis em todo o mundo. Mas tavez você não saiba que o seu simpático mascote tem um nome e uma história muito curiosa? Pois acompanhe esse artigo para aprender muita coisa sobre esse robozinho.</p>
