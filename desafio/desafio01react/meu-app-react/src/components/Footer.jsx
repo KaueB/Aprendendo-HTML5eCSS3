30fcfb9faf6041014fe38d266d95562e757271f1
Kaue Batista <83407339+KaueB@users.noreply.github.com>
2021-12-21 11:50:09 -0300

    fazendo em react

--- desafio/desafio01react/meu-app-react/src/components/Footer.jsx
+++ desafio/desafio01react/meu-app-react/src/components/Footer.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function Footer() {
     return (
-        <div>
-            teste
-        </div>
+        <footer>
+            <p>Site criado por <a href="https://github.com/gustavoguanabara" target="_blank">Gustavo Guanabara</a> para o <a href="https://www.cursoemvideo.com/matricule-se-agora/" target="_blank">CursoemVideo</a>.</p>
+        </footer>
     );
 };
 
--- desafio/desafio01react/meu-app-react/src/components/header.jsx
+++ desafio/desafio01react/meu-app-react/src/components/header.jsx
@@ -4,10 +4,7 @@ function Header() {
     return (
         <header>
             <h1>Curiosidades de Tecnologia</h1>
-            <p id='subtitulo'>
-                Tudo aquilo que você sempre quis saber sobre o mundo Tech, em um único lugar
-            </p>
-            
+            <p>Tudo aquilo que você sempre quis saber sobre o mundo Tech, em um único lugar.</p>
         </header> 
     );
 }
