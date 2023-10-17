63f43a26ae2ca661cb474ff17a1fb05887f33e82
Kaue Batista da Silva <83407339+KaueB@users.noreply.github.com>
2023-10-16 22:44:31 -0300

    conseguindo

--- PraticasHTMLeCSS/HMTL calculadora/script.js
+++ PraticasHTMLeCSS/HMTL calculadora/script.js
@@ -1,34 +1,82 @@
-var numberResult = document.getElementById('result').innHTML;
+let result = document.getElementById('result');
 
 let number01 = 0;
 let number02 = 0;
 
 function appendToResult(value) {
-  document.getElementById('result').innerHTML = numberResult + value;
-
-  number01 = Number(number01 + value)
-  console.log(number01)
-};
+  let lastCharacter = result.innerHTML.slice(-1);
+  if (value != '.') {
+    result.innerHTML += value;
+  } else if (lastCharacter != '.' && lastCharacter != '') {
+    result.innerHTML += value;
+  }
+}
 
 function operation(value) {
-
-  document.getElementById('result').innerHTML += value
-  
-  
-  let lastCharacter = numberResult.value.slice(-1)
-  if (lastCharacter == '-' || '+' || '*' || '/') {
-    document.getElementById('result').innerHTML += ''
+  let lastCharacter = result.innerHTML.slice(-1);
+  let firstCharacter = result.innerHTML.slice(0)
+  if (result.innerHTML === '' && value !== '-') {
+    result.innerHTML = '';
+  } else if (firstCharacter === '+' && firstCharacter === '*' && firstCharacter === '/') {
+    result.innerHTML = '';
+  } else if (lastCharacter !== '-' && lastCharacter !== '+' && lastCharacter !== '*' && lastCharacter !== '/') {
+    result.innerHTML += value;
   } else {
-    document.getElementById('result').innerHTML += value
-  };
-};
+    clearNumber();
+    result.innerHTML += value;
+  }
+}
 
 function clearResult() {
-  document.getElementById('result').innerHTML = ''
-  number01 = 0
-  number02 = 0
-};
+  result.innerHTML = '';
+  number01 = 0;
+  number02 = 0;
+}
 
 function clearNumber() {
+  result.innerHTML = result.innerHTML.slice(0, -1);
+}
 
-};
+function calculateResult() {
+  let getOperation = '';
+  let calculateAux = '';
+  if (result.innerHTML.indexOf('-') !== -1 && result.innerHTML.slice(0) === '-') {
+    getOperation = '-';
+    calculateAux = result.innerHTML.indexOf('-');
+  } else if (result.innerHTML.indexOf('+') !== -1) {
+    getOperation = '+';
+    calculateAux = result.innerHTML.indexOf('+');
+  } else if (result.innerHTML.indexOf('*') !== -1) {
+    getOperation = '*';
+    calculateAux = result.innerHTML.indexOf('*');
+  } else if (result.innerHTML.indexOf('/') !== -1) {
+    getOperation = '/';
+    calculateAux = result.innerHTML.indexOf('/');
+  }
+
+  number01 = Number(result.innerHTML.slice(0, calculateAux));
+  number02 = Number(result.innerHTML.slice(calculateAux + 1));
+
+  switch (getOperation) {
+    case '-':
+      result.innerHTML = `${number01 - number02}`
+      break;
+    case '+':
+      result.innerHTML = `${number01 + number02}`
+      break
+    case '*':
+      result.innerHTML = `${number01 * number02}`
+      break
+    case '/':
+      result.innerHTML = `${number01 / number02}`
+      break
+    default:
+      break;
+  }
+
+  /* console.clear()
+  console.log(getOperation);
+  console.log(calculateAux);
+  console.log(number01);
+  console.log(number02); */
+}
